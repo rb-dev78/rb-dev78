5e042ceef93bfebc2d4206622d8908b37d630c51
Rob Bush <robbus2415@students.ecpi.edu>
2026-03-02 20:23:25 -0500

    Add files via upload
    CYB120L 1.3 assignment

---
+++ Part-1-Task-1-1.py
@@ -0,0 +1,49 @@
+## Task 1.1
+#print("===SYSTEM LOG===")
+#print("User: ROBBUS2415 logging into system")
+#print("ACCESS GRANTED: ROBBUS2415")
+#print("Time: 1:07 PM")
+
+## Task 2.1
+#print("===SYSTEM LOG===) ## Broken code: missing closing quotation mark.
+
+## Task 2.2
+#print("User name: Rob")
+#print("System name: Windows 11 Pro")
+#print("Date and time: 03/02/2026")
+
+## Task 2.3
+# Display user and system information
+#print("User name: Rob")
+#print("System name: Windows 11 Pro")
+#print("Date and time: 03/02/2026")
+
+## Task 3.1
+#message = "Security Alert"
+#print(message)
+
+## Task 3.2
+#message = "You've been hacked!"
+#print(message)
+
+## Task 3.3
+#1message = "You've been hacked!"
+#print(1message)
+#message1 = "You've been hacked!"  ## fixed code
+#print(message1)                   ## fixed code
+
+## Task 4.1
+
+#name = input("Enter your username.")
+#print("=======================================")
+#print(f"Welcome to ECPI Secure Shell, {name}")
+#print("=======================================")
+
+## Task 4.2
+name = input("Enter your username.")
+depart = input("Enter the department name")
+print("==================================================")
+print(f"Welcome to ECPI Secure Shell, {name} - {depart}")
+print("==================================================")
+
+    
