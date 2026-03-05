cb79abbe541fdb00f09f54ae7b82c1182c2dda08
Rob Bush <robbus2415@students.ecpi.edu>
2026-03-04 19:24:24 -0500

    Python Input & Output
    CYB120L

---
+++ robbus2415-Python-Input&Output.py
@@ -0,0 +1,35 @@
+#########################################
+## Assessment: Python Input and Output ##
+##   Robert Bush - CYB120L - 3/4/2026  ##
+#########################################
+
+## Part I. Input and Output Basics ##
+
+## Task 1.2 Observe
+###################
+## name = input("What is your name? ")
+## print("Hello, " + name + "!")
+
+## Task 1.3 Analyze
+###################
+## name = input("What is your name? ")
+## print(type(name))
+
+## Task 1.4 Experiment
+######################
+## name = input("What is your name? ")
+## print("Hello," , name)
+## print(type(name))
+
+## Task 1.5 Challenge Thinking
+##############################
+## number = int(input("Enter first number: "))
+## number2 = int(input("Enter second number: "))
+## print("Total number:", number + number2)
+
+## Task  1.6 Extend
+###################
+name = input("What is your name? ")
+age = input("What is your age? ")
+print("Hello " + name + ", you are " + age + "!")
+
