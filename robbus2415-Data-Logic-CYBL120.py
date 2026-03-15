6b4356ed7d7d54fefb600478eee3cea8a9aa4734
Rob Bush <robbus2415@students.ecpi.edu>
2026-03-15 09:14:23 -0400

    Add files via upload

---
+++ robbus2415-Data-Logic-CYBL120.py
@@ -0,0 +1,163 @@
+########################################
+##  Assessment: Python Data and Logic ##
+##       ROOBUS2415 - CYB120L         ##
+########################################
+
+#########################
+## Part 1: Data Types  ##
+#########################
+
+## 1. Predict
+## On document
+
+## 2. Observe
+
+#print(type("42"))
+#print(type(42))
+#print(type(3.14))
+#print(type(True))
+
+## 3. Analyze
+
+#print("42" + "8") #strings
+#print(42 + 8) #integer
+#print(int("42") + 8)
+
+## 4. Experiment
+
+#print(int("welcome"))
+#print(int("42"))
+
+## 6. Extend
+## On document
+
+#######################
+## Part 2: Operators ##
+#######################
+
+## 1. Predict
+## On document
+
+## 2. Observe
+
+#print(3 + 4 * 2)
+#print(3 + 4) * 2)
+#print("3" + "4")
+#print("3" + 4)
+
+## 3. Analyze
+
+#print(5 == 5)
+#print(5 != 3)
+#print("hi" == "hi")
+
+## 4. Experiment
+
+#print(10 / 3)
+#print(10 // 3)
+#print(210.0 / 3.0)
+#print(10 % 3)
+
+## 5. Challenge Thinking
+## On document
+
+## 6. Extend
+## On document
+
+#####################################
+## Part 3: Boolean Logic & If/Else ##
+#####################################
+
+## 1. Predict
+## On document
+
+## 2. Observe
+
+#x = 3
+#if x > 5:
+#    print("Greater than 5")
+#else:
+#    print("5 or less")
+
+## 3. Analyze
+
+#x = 0
+#if x:
+#    print("truthy")
+#else:
+#    print("falsy")
+#s = ""
+#if s:
+#    print("truthy")
+#else:
+#    print("falsy")
+
+## 4. Experiment
+
+#age = 19
+#has_ticket = True
+#if age >= 18 and has_ticket:
+#    print("Adult entry")
+#elif age >= 13 or has_ticket:
+#    print("Teen or ticket holder")
+#else:
+#    print("Child entry")
+
+## 5. Challenge Thinking
+## On document
+
+## 6. Extend
+
+#login_ok = False
+#if login_ok:
+#    print("Access granted")
+#else:
+#    print("Access denied")
+#score = 59
+#if score >= 90:
+#    grade = "A"
+#elif score >= 80:
+#    grade = "B"
+#elif score >= 70:
+#    grade = "C"
+#else:
+#    grade = "F"
+#print(grade)
+
+#########################
+## Part 4: Error Types ##
+#########################
+
+## 1. Predict
+
+#print("The answer is: " + 42)
+
+## 2. Observe
+## On document
+
+## 3. Analyze
+## On document
+
+## 4. Experiment
+#prnt("hi")
+#print(total)
+
+## 5. Challenge Thinking
+## On document
+
+## 6. Extend
+
+try:
+    age = int(input("Enter your age:"))
+    print(f"Next year you will be {age + 1}.")
+except ValueError:
+    print("Please enter a number, not text.")
+
+
+
+
+
+
+    
+    
+
